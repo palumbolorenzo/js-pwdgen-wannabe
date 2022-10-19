d4275e0b439c2d491c53cbfcaf50dad0cf606569
palumbolorenzo <palumbolorenzo97@gmail.com>
2022-10-19 17:38:22 +0200

    Provvisorio

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let nome = prompt('Qual è il tuo nome?');
+let nomeUser = nome;
+document.getElementById('name').innerHTML = nomeUser;
+console.log(nomeUser);
+
+let cognome = prompt('Qual è il tuo cognome?');
+let cognomeUser = cognome;
+document.getElementById('surname').innerHTML = cognomeUser;
+console.log(cognomeUser);
+
+let colore = prompt('Qual è il tuo colore preferito?');
+let coloreUser = colore;
+document.getElementById('color').innerHTML = coloreUser;
+console.log(coloreUser);
+
+
+
+/*let età = parseInt(prompt('Quanti anni hai?'));
+let annoNascita = 2022 - età;
+document.getElementById('year').innerHTML = annoNascita;
+console.log(annoNascita)*/
